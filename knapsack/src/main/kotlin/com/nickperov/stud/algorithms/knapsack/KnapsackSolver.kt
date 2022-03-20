647b13ce18171b225516d2b48ecd7b7c13e5fe8c
nickperov <nikolay.perov@gmail.com>
2022-03-20 23:02:35 +1300

    add knapsack solver

---
+++ knapsack/src/main/kotlin/com/nickperov/stud/algorithms/knapsack/KnapsackSolver.kt
@@ -0,0 +1,11 @@
+package com.nickperov.stud.algorithms.knapsack
+
+interface KnapsackSolver {
+    fun solve(size: Integer, items: List<Item>)
+}
+
+class KnapsackBFSolver : KnapsackSolver {
+    override fun solve(size: Integer, items: List<Item>) {
+        TODO("Not yet implemented")
+    }
+}
